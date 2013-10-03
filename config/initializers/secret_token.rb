6ff8fa0be8593d980514e10a1f7a94e2803f422e
Jaroslav <support@gglit.me>
2013-10-04 01:49:12 +0400

    Commit Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '344042b5edec345a8aee7e399f52d8712f3645cd9029f6f3eaa72f164ca8536b0e8e7886194186a629b3182c973929542c0747e68b18efefb33acc03ef1896f0'
